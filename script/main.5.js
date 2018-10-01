f9cc96ae3610eb19dd1df6ed5043e1b17354791e
Ken Leung <github@iken.be>
2018-09-30 19:51:15 -0700

    polyfills

--- script/main.5.js
+++ script/main.5.js
@@ -20,7 +20,7 @@ function main3() {
     $('.starter').click(getStart);
 
     $('goFS').click(function() {
-        document.body.requestFullscreen().then(getStart);
+        toggleFullScreen()
     }, false)
 
     var startx = 0;
@@ -345,6 +345,20 @@ function main3() {
                     gridMove("down");
                 });
         */
+    }
+
+    function toggleFullScreen() {
+        var doc = window.document;
+        var docEl = doc.documentElement;
+
+        var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+        var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+        if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+            requestFullScreen.call(docEl);
+        } else {
+            cancelFullScreen.call(doc);
+        }
     }
 
 
